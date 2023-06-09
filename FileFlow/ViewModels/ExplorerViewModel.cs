9fbfd7cc74e32cec862b4a714d1cd3a58fec3ba6
REDIZIT <38188793+REDIZIT@users.noreply.github.com>
2023-06-09 17:28:38 +0300

    SettingsWindow design

--- FileFlow/ViewModels/ExplorerViewModel.cs
+++ FileFlow/ViewModels/ExplorerViewModel.cs
@@ -33,9 +33,6 @@ namespace FileFlow.Views
         public void Initialize()
         {
             Tabs.Add(new TabViewModel(this, fileSystem, iconExtractor, "C:\\Tests"));
-            Tabs.Add(new TabViewModel(this, fileSystem, iconExtractor, "C:\\Tests"));
-            Tabs.Add(new TabViewModel(this, fileSystem, iconExtractor, "C:\\Tests"));
-            Tabs.Add(new TabViewModel(this, fileSystem, iconExtractor, "C:\\Tests"));
 
             OnTabClicked(Tabs[0]);
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Tabs)));
